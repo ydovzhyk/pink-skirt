0360b03d80086f793d911d560d7b67439ce283b6
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2025-07-31 18:53:18 +0300

    Update index.jsx

--- src/components/shared/image-banner/index.jsx
+++ src/components/shared/image-banner/index.jsx
@@ -1,8 +1,35 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import Text from '../text/text';
+import { getSections } from '../../header/navigation/navigation';
 
 const ImageBanner = () => {
+
+  const sections = getSections();
+  const router = useRouter();
+
+  const handleNavigate = id => {
+    const section = sections.find(sec => sec.id === id);
+    const yOffset = section?.offset;
+
+    router.push(`/#${id}`);
+
+    setTimeout(() => {
+      const element = document.getElementById(id);
+
+      if (element && yOffset !== undefined) {
+        const y =
+          element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+        window.scrollTo({
+          top: y,
+          behavior: 'smooth',
+        });
+      }
+    }, 50);
+  };
+
   return (
     <div className="relative w-full h-[300px] md:h-[350px] lg:h-[500px] flex flex-row border-t border-b border-gray-200">
       {/* Ліва частина */}
@@ -23,7 +50,7 @@ const ImageBanner = () => {
 
       {/* Текст по центру банера */}
       <div className="absolute inset-0 flex items-center justify-center px-4">
-        <div className="max-w-[300px]">
+        <div className="max-w-[300px] flex flex-col items-center gap-10">
           <Text
             type="title"
             as="p"
@@ -33,6 +60,25 @@ const ImageBanner = () => {
           >
             Enduring charm, playfully modern
           </Text>
+          <button
+            className="group"
+            role="button"
+            onClick={() => handleNavigate('contacts')}
+          >
+            <div
+              style={{ borderWidth: '0.5px' }}
+              className="flex items-center gap-1 group-hover:gap-3 px-3 md:px-8 py-3 md:py-4 rounded-md bg-white border-gray-400 tracking-wider transition-all duration-300 ease-out bg-transparent group-hover:bg-[#F8F1F1] group-hover:border-[#F8F1F1] group-hover:shadow-md w-full btn-shine uppercase"
+            >
+              <Text
+                type="tiny"
+                as="span"
+                fontWeight="light"
+                className="text-[var(--text-title)]"
+              >
+                Contact Me
+              </Text>
+            </div>
+          </button>
         </div>
       </div>
     </div>
